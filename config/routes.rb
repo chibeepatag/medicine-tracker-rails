7970106c3c4982ce2eaae2c103947926f8d05dbc
unknown <marie.sheridan.tuazon@gmail.com>
2018-05-30 20:33:00 +0800

    Added controller for organs.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   post 'report/email'
 
   root to: 'patients#index'
-  
+
   resources :patients do
   	resources :events
   	resources :medicines
   end
+
+  resources :organs, only: [:index] do
+      resources :reactions, only: [:index]
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
