9e8a412cc05cf571a5ae3c3ad492af78f49aa322
unknown <marie.sheridan.tuazon@gmail.com>
2018-05-30 20:51:32 +0800

    Added more routes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,13 @@ Rails.application.routes.draw do
   end
 
   resources :organs, only: [:index] do
-      resources :reactions, only: [:index]
+    resources :reactions, only: [:index]
   end
-  
+
+  resources :antibiotics, only: [:index]
+  resources :doses, only: [:index]
+  resources :severities, only: [:index]
+  resources :frequencies, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
