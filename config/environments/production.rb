2f0a2419b63bd8f1e16fbf13a4002c10227e6c5d
unknown <marie.sheridan.tuazon@gmail.com>
2018-06-04 13:16:22 +0800

    Disable js compression for now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -97,6 +97,6 @@ Rails.application.configure do
     user_name:            'easiripple',
     password:             'balmaidcalendula',
     authentication:       'plain',
-    enable_starttls_auto: true  
+    enable_starttls_auto: true
   }
 end
