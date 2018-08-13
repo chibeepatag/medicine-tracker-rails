f6a241dd480765d0f1850688659021025270e52f
cpatag <celine@imonggo.com>
2018-08-13 19:56:27 +0800

    email via app

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -11,10 +11,9 @@ class ReportController < ApplicationController
   def sendEmailFromApp
       @email = patient_params[:email]
 
-      #send email logic here, return json with "message". The message will be shown in the app.
-      #example: if (success) { message = successfull message } else { message = error message }
+      ReportMailer.with(patient_id: patient_params[:patient_id], email: @email).send_report.deliver_now
 
-      render json: {"message": "Not yet implemented.\n\nSample message: Report for " + @patient.name + " was successfully sent to " + @email + "."}
+      render json: {"message": "Report for " + @patient.name + " was successfully sent to " + @email + "."}
   end
 
   private
