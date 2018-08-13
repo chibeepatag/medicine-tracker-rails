169aec89047c714f4487f4d10e0be03c9ac0921d
cpatag <celine@imonggo.com>
2018-08-13 20:10:14 +0800

    default mailer url production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,6 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
+
+  config.action_mailer.default_url_options = { :host => "polar-reaches-96790.herokuapp.com" }
 end
