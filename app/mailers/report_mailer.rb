30889bd4c928765f6cf1f032db0662a66709fee4
cpatag <celine@imonggo.com>
2018-08-13 10:41:47 +0800

    email with link to report

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,10 +1,8 @@
 class ReportMailer < ApplicationMailer
-
 	def send_report
 		email = params[:email]
 		@patient = Patient.find params[:patient_id]
 
-		attachments['README.md'] = File.read('README.md')
-		mail(to: email, subject: "Medicine Reaction of #{@patient.name}" )
+		mail(to: email, subject: "Medicine Reaction of #{@patient.name}")
 	end
 end
--- test/mailers/previews/report_mailer_preview.rb
+++ test/mailers/previews/report_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/report_mailer
 class ReportMailerPreview < ActionMailer::Preview
-
+	def send_report
+		params = {patient_id: 1, email: 'chibeepatag@yahoo.com'}
+		
+		ReportMailer.with(params).send_report
+	end
 end
